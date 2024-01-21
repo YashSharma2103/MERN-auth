a984a2b82bf128d8ac58ef2b187182651557caea
Yash Sharma <yash2003.ys@gmail.com>
2024-01-21 13:50:21 +0530

    created pages and header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import React,{ useState } from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 function App() {
 
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
